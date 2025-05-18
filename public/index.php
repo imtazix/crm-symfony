bf3331ab045e01690d3e20dd633c2264b7ab659b
ELMAHDI29 <100849542+ELMAHDI29@users.noreply.github.com>
2025-05-18 17:46:59 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Kernel;
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\InMemory;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+// 🔧 Initialisation du registre Prometheus (OBLIGATOIRE pour que /metrics fonctionne)
+CollectorRegistry::setDefault(new CollectorRegistry(new InMemory()));
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
